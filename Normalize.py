603ac413ded616951132b10ea35fd6ff459f1a4b
Mikhail <tumisha22@gmail.com>
2022-11-01 19:03:41 +0300

    First commit

---
+++ Normalize.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import sqlite3
+import numpy as np
+import itertools
+from sklearn import preprocessing
+
+
+con = sqlite3.connect("C:/Users/MikhailAnd/Desktop/Result/DATA.db")
+df = pd.read_sql("SELECT * FROM Positive", con)
+
+
+#F_frag
+F_frag = pd.read_sql('SELECT F_frag FROM Positive', con)
+_F_frag1 = np.array(F_frag)
+_F_frag = list(itertools.chain(*_F_frag1))
+n_F_frag1 = preprocessing.normalize([_F_frag])
+n_F_frag = list(itertools.chain(*n_F_frag1))
+
+
+#STETEOF_1_frag
+STETEOF_1_frag = pd.read_sql('SELECT STETEOF_1_frag FROM Positive', con)
+_STETEOF_1_frag1 = np.array(STETEOF_1_frag)
+_STETEOF_1_frag = list(itertools.chain(*_STETEOF_1_frag1))
+n_STETEOF_1_frag1 = preprocessing.normalize([_STETEOF_1_frag])
+n_STETEOF_1_frag = list(itertools.chain(*n_STETEOF_1_frag1))
+
+#STETEOF_2_frag
+STETEOF_2_frag = pd.read_sql('SELECT STETEOF_2_frag FROM Positive', con)
+_STETEOF_2_frag1 = np.array(STETEOF_2_frag)
+_STETEOF_2_frag = list(itertools.chain(*_STETEOF_2_frag1))
+n_STETEOF_2_frag1 = preprocessing.normalize([_STETEOF_2_frag])
+n_STETEOF_2_frag = list(itertools.chain(*n_STETEOF_2_frag1))
+
+#STETEO_frag
+STETEO_frag = pd.read_sql('SELECT STETEO_frag FROM Positive', con)
+_STETEO_frag1 = np.array(STETEO_frag)
+_STETEO_frag = list(itertools.chain(*_STETEO_frag1))
+n_STETEO_frag1 = preprocessing.normalize([_STETEO_frag])
+n_STETEO_frag = list(itertools.chain(*n_STETEO_frag1))
+
+#STEZCRTEO_1_frag
+STEZCRTEO_1_frag = pd.read_sql('SELECT STEZCRTEO_1_frag FROM Positive', con)
+_STEZCRTEO_1_frag1 = np.array(STEZCRTEO_1_frag)
+_STEZCRTEO_1_frag = list(itertools.chain(*_STEZCRTEO_1_frag1))
+n_STEZCRTEO_1_frag1 = preprocessing.normalize([_STEZCRTEO_1_frag])
+n_STEZCRTEO_1_frag = list(itertools.chain(*n_STEZCRTEO_1_frag1))
+
+#STEZCRTEO_2_frag
+STEZCRTEO_2_frag = pd.read_sql('SELECT STEZCRTEO_2_frag FROM Positive', con)
+_STEZCRTEO_2_frag1 = np.array(STEZCRTEO_2_frag)
+_STEZCRTEO_2_frag = list(itertools.chain(*_STEZCRTEO_2_frag1))
+n_STEZCRTEO_2_frag1 = preprocessing.normalize([_STEZCRTEO_2_frag])
+n_STEZCRTEO_2_frag = list(itertools.chain(*n_STEZCRTEO_2_frag1))
+
+
+#STE_frag
+STE_frag = pd.read_sql('SELECT STE_frag FROM Positive', con)
+_STE_frag1 = np.array(STE_frag)
+_STE_frag = list(itertools.chain(*_STE_frag1))
+n_STE_frag1 = preprocessing.normalize([_STE_frag])
+n_STE_frag = list(itertools.chain(*n_STE_frag1))
+
+
+#ZCRTEO_frag
+ZCRTEO_frag = pd.read_sql('SELECT ZCRTEO_frag FROM Positive', con)
+_ZCRTEO_frag1 = np.array(ZCRTEO_frag)
+_ZCRTEO_frag = list(itertools.chain(*_ZCRTEO_frag1))
+n_ZCRTEO_frag1 = preprocessing.normalize([_ZCRTEO_frag])
+n_ZCRTEO_frag = list(itertools.chain(*n_ZCRTEO_frag1))
+
+#ZCR_frag
+ZCR_frag = pd.read_sql('SELECT ZCR_frag FROM Positive', con)
+_ZCR_frag1 = np.array(ZCR_frag)
+_ZCR_frag = list(itertools.chain(*_ZCR_frag1))
+n_ZCR_frag1 = preprocessing.normalize([_ZCR_frag])
+n_ZCR_frag = list(itertools.chain(*n_ZCR_frag1))
+
+with sqlite3.connect(r'C:/Users/MikhailAnd/Desktop/Result/DATAn.db') as con:
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS Positive(
+    F_frag REAL, 
+    STETEOF_1_frag REAL, 
+    STETEOF_2_frag REAL, 
+    STETEO_frag REAL, 
+    STEZCRTEO_1_frag REAL,
+    STEZCRTEO_2_frag REAL,
+    STE_frag REAL,
+    ZCRTEO_frag REAL,
+    ZCR_frag REAL )''')
+
+print(len(n_F_frag))
+print(len(n_STETEOF_1_frag))
+print(len(n_STETEOF_2_frag))
+print(len(n_STETEO_frag))
+print(len(n_STEZCRTEO_1_frag))
+print(len(n_STEZCRTEO_2_frag))
+print(len(n_STE_frag))
+print(len(n_ZCRTEO_frag))
+print(len(n_ZCR_frag))
+
+for i3 in range(0,len(_F_frag)):
+    c = con.cursor()
+    c.execute('''INSERT INTO Positive(F_frag, STETEOF_1_frag , STETEOF_2_frag, STETEO_frag, STEZCRTEO_1_frag,
+    STEZCRTEO_2_frag,STE_frag,ZCRTEO_frag,ZCR_frag) VALUES(?,?,?,?,?,?,?,?,?)''',(n_F_frag[i3], n_STETEOF_1_frag[i3], n_STETEOF_2_frag[i3], n_STETEO_frag[i3], n_STEZCRTEO_1_frag[i3], n_STEZCRTEO_2_frag[i3], n_STE_frag[i3], n_ZCRTEO_frag[i3], n_ZCR_frag[i3]))
+con.commit()
---
+++ main.py
@@ -0,0 +1,104 @@
+import numpy as np
+import scipy.io as sc
+import os
+import itertools
+import sqlite3
+
+path = 'C:/Users/MikhailAnd/Desktop/Новая папка/Сигналы/Положительные сигналы'
+for dirs, folder, files in os.walk(path):
+    print('Выбранный каталог: ', dirs)
+    print('Вложенные папки: ', folder)
+    break
+
+_F_frag = np.array([])
+_STETEOF_1_frag = np.array([])
+_STETEOF_2_frag = np.array([])
+_STETEO_frag = np.array([])
+_STEZCRTEO_1_frag = np.array([])
+_STEZCRTEO_2_frag = np.array([])
+_STE_frag = np.array([])
+_ZCRTEO_frag = np.array([])
+_ZCR_frag = np.array([])
+
+for i in folder:
+    pathh = 'C:/Users/MikhailAnd/Desktop/Новая папка/Сигналы/Положительные сигналы/' + i + '/results/result_pref_name_settinng_AI_1_ai.mat'
+    mat = sc.loadmat(pathh)
+    if os.path.exists('C:/Users/MikhailAnd/Desktop/Новая папка/Сигналы/Положительные сигналы/' + i + '/results/result_pref_name_settinng_AI_1_ai.mat'):
+
+        a = mat['F_frag_s']
+        aa = list(itertools.chain(*a))
+        _F_frag1 = np.array(aa)
+        _F_frag = np.append(_F_frag, _F_frag1)
+
+        b = mat['STETEOF_1_frag_s']
+        bb = list(itertools.chain(*b))
+        _STETEOF_1_frag1 = np.array(bb)
+        _STETEOF_1_frag = np.append(_STETEOF_1_frag, _STETEOF_1_frag1)
+
+        c = mat['STETEOF_2_frag_s']
+        cc = list(itertools.chain(*c))
+        _STETEOF_2_frag1 = np.array(cc)
+        _STETEOF_2_frag = np.append(_STETEOF_2_frag, _STETEOF_2_frag1)
+
+        d = mat['STETEO_frag_s']
+        dd = list(itertools.chain(*d))
+        _STETEO_frag1 = np.array(dd)
+        _STETEO_frag = np.append(_STETEO_frag, _STETEO_frag1)
+
+        e = mat['STEZCRTEO_1_frag_s']
+        ee = list(itertools.chain(*e))
+        _STEZCRTEO_1_frag1 = np.array(ee)
+        _STEZCRTEO_1_frag = np.append(_STEZCRTEO_1_frag, _STEZCRTEO_1_frag1)
+
+        f = mat['STEZCRTEO_2_frag_s']
+        ff = list(itertools.chain(*f))
+        _STEZCRTEO_2_frag1 = np.array(ff)
+        _STEZCRTEO_2_frag = np.append(_STEZCRTEO_2_frag, _STEZCRTEO_2_frag1)
+
+        g = mat['STE_frag_s']
+        gg = list(itertools.chain(*g))
+        _STE_frag1 = np.array(ff)
+        _STE_frag = np.append(_STE_frag, _STE_frag1)
+
+        k = mat['ZCRTEO_frag_s']
+        kk = list(itertools.chain(*k))
+        _ZCRTEO_frag1 = np.array(kk)
+        _ZCRTEO_frag = np.append(_ZCRTEO_frag, _ZCRTEO_frag1)
+
+        l = mat['ZCR_frag_s']
+        ll = list(itertools.chain(*l))
+        _ZCR_frag1 = np.array(ll)
+        _ZCR_frag = np.append(_ZCR_frag, _ZCR_frag1)
+    else:
+        continue
+
+
+#БД
+with sqlite3.connect(r'C:/Users/MikhailAnd/Desktop/Result/DAT.db') as con:
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS Positive(
+    F_frag REAL, 
+    STETEOF_1_frag DOUBLE, 
+    STETEOF_2_frag REAL, 
+    STETEO_frag REAL, 
+    STEZCRTEO_1_frag REAL,
+    STEZCRTEO_2_frag REAL,
+    STE_frag REAL,
+    ZCRTEO_frag REAL,
+    ZCR_frag REAL )''')
+
+print(len(_F_frag))
+print(len(_STETEOF_1_frag))
+print(len(_STETEOF_2_frag))
+print(len(_STETEO_frag))
+print(len(_STEZCRTEO_1_frag))
+print(len(_STEZCRTEO_2_frag))
+print(len(_STE_frag))
+print(len(_ZCRTEO_frag))
+print(len(_ZCR_frag))
+
+for i3 in range(0,len(_F_frag)):
+    c = con.cursor()
+    c.execute('''INSERT INTO Positive(F_frag, STETEOF_1_frag , STETEOF_2_frag, STETEO_frag, STEZCRTEO_1_frag,
+    STEZCRTEO_2_frag,STE_frag,ZCRTEO_frag,ZCR_frag) VALUES(?,?,?,?,?,?,?,?,?)''',(_F_frag[i3], _STETEOF_1_frag[i3], _STETEOF_2_frag[i3], _STETEO_frag[i3], _STEZCRTEO_1_frag[i3],_STEZCRTEO_2_frag[i3],_STE_frag[i3],_ZCRTEO_frag[i3],_ZCR_frag[i3]))
+con.commit()
